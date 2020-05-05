422371599209822566ef5dcf95e266f4f2995bad
Bozó Tamás Dániel <btd2k17@gmail.com>
2020-05-05 12:29:20 +0200

    Kártyás és készpénzfizetéshez szükséges osztályok létrehozva '.idea' mappa automatikus létrehozásra került az IntelliJ program használata miatt

---
+++ Progtehc_bead/src/classes/BankCardInfo.java
@@ -0,0 +1,10 @@
+package com.classes;
+
+/**
+*Ezt az osztályt fogom arra használni, hogy leelenőrizzem, hogy érvényes bankkártyával akar-e fizetni a kliens
+*A kártya hitelességét egy egyszerű ellenőrzéssel fogjuk ellenőrizni. (van-e benne betű)
+*/
+
+public class BankCardInfo {
+
+}
---
+++ Progtehc_bead/src/classes/CashInfo.java
@@ -0,0 +1,8 @@
+ package com.classes;
+ /*
+ *Ez az osztály akkor lesz használva ha a kölcsönző személy készpénzzel akar fizetni
+ */
+ public class CashInfo {
+	 
+ }
+ 
