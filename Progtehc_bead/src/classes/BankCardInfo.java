128bf39149f5b4b755b0315c538b401f1448d21e
Bozó Tamás Dániel <btd2k17@gmail.com>
2020-05-05 12:37:28 +0200

    Interfész a fizetési módhoz létrehozva, valamint az adott osztályokban az ehhez tartozó override metódus megírva

--- Progtehc_bead/src/classes/BankCardInfo.java
+++ Progtehc_bead/src/classes/BankCardInfo.java
@@ -1,10 +1,18 @@
 package com.classes;
 
+import com.Interfaces.IPayementInfo;
 /**
 *Ezt az osztályt fogom arra használni, hogy leelenőrizzem, hogy érvényes bankkártyával akar-e fizetni a kliens
 *A kártya hitelességét egy egyszerű ellenőrzéssel fogjuk ellenőrizni. (van-e benne betű)
 */
 
-public class BankCardInfo {
+public class BankCardInfo implements IPayementInfo {
+	@Override
+	public String info() {
+		return "credit card";
+	}
+	/**
+	* Az interrfészben található metódus értékét felülírja.
+	*/
 
 }
--- Progtehc_bead/src/classes/CashInfo.java
+++ Progtehc_bead/src/classes/CashInfo.java
@@ -1,8 +1,14 @@
  package com.classes;
+ 
+ import com.Interfaces.IPaymentInfo;
  /*
  *Ez az osztály akkor lesz használva ha a kölcsönző személy készpénzzel akar fizetni
  */
- public class CashInfo {
-	 
+ public class CashInfo implements IPaymentInfo {
+	@Override
+	public String info() {
+		return "cash";
+	}
+	//Felülírjuk az interfészben található metódus visszatérési értékét.
  }
  
