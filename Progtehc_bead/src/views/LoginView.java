2523e06407626fdc4d66e89e3976261839320e1b
Bozó Tamás Dániel <btd2k17@gmail.com>
2020-05-10 21:25:05 +0200

    Bejelentkező felület

---
+++ Progtehc_bead/src/views/LoginView.java
@@ -0,0 +1,38 @@
+package com.views;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class LoginView extends JFrame {
+    private JPasswordField password_PF;
+    private JTextField username_TF;
+    private JButton btn_login;
+    private JButton btn_register;
+    private JPanel mainPanel;
+    private JLabel lbl_message;
+
+    public String getUsername(){
+        return this.username_TF.getText();
+    }
+    public String getPassword(){
+        return new String(this.password_PF.getPassword());
+    }
+
+    public void setLoginActionListener(ActionListener listener){
+        this.btn_login.addActionListener(listener);
+    }
+    public void setRegisterActionListener(ActionListener listener){
+        this.btn_register.addActionListener(listener);
+    }
+    public void setLabelMessage(String message){
+        lbl_message.setText(message);
+    }
+
+    public  LoginView(){
+        add(mainPanel);
+        setTitle("Autókereskedés");
+        setResizable(false);
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
