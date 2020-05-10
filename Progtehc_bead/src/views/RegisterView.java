687ce8d06ccc28921c11b084ccffc97ce3cbca95
Bozó Tamás Dániel <btd2k17@gmail.com>
2020-05-10 22:01:52 +0200

    Regisztráló ablak

---
+++ Progtehc_bead/src/views/RegisterView.java
@@ -0,0 +1,50 @@
+package com.views;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class RegisterView extends JFrame {
+    private JPanel regMain;
+    private JTextField username_TF;
+    private JPasswordField password_PF;
+    private JTextField email_TF;
+    private JTextField firstname_TF;
+    private JTextField lastname_TF;
+    private JTextField address_TF;
+    private JButton registerButton;
+    private JLabel lbl_res;
+
+    public String getUsername(){
+        return this.username_TF.getText();
+    }
+    public String getPassword(){
+        return new String(this.password_PF.getPassword());
+    }
+    public String getEmail(){
+        return this.email_TF.getText();
+    }
+    public String getFirstName(){
+        return this.firstname_TF.getText();
+    }
+    public String getLastName(){
+        return this.lastname_TF.getText();
+    }
+    public String getAddress(){
+        return this.address_TF.getText();
+    }
+
+    public void setRegisterActionListener(ActionListener listener){
+        this.registerButton.addActionListener(listener);
+    }
+    public void setResult(String result){
+        this.lbl_res.setText(result);
+    }
+
+    public RegisterView(){
+        add(regMain);
+        setTitle("Regisztráció");
+        setResizable(false);
+        setSize(500, 400);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    }
+}
