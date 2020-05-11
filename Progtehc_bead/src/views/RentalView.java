049ac96522528ef9e00d2c4ff7fa8b0ab38fa38e
Bozó Tamás Dániel <btd2k17@gmail.com>
2020-05-11 08:38:25 +0200

    View és napló

---
+++ Progtehc_bead/src/views/RentalView.java
@@ -0,0 +1,67 @@
+package com.views;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionListener;
+
+public class RentalView extends JFrame{
+    private JTable rental_table;
+    private JButton btn_rent;
+    private JLabel lbl_priceOfRent;
+    private JLabel lbl_price;
+    private JLabel lbl_rental;
+    private JPanel RentalMain;
+    private JButton btn_check;
+
+    public void setPriceLabel(String price){
+        this.lbl_price.setText(price);
+    }
+    public void setRentalLabel(String state){
+        this.lbl_rental.setText(state);
+    }
+    public void setRentButton(ActionListener listener){
+        this.btn_rent.addActionListener(listener);
+    }
+    public void setBtn_checkActionListener(ActionListener listener){
+        this.btn_check.addActionListener(listener);
+    }
+
+    public JTable getRental_table() {
+        return rental_table;
+    }
+
+    public void setRental_table(JTable rental_table) {
+        this.rental_table = rental_table;
+    }
+
+    public RentalView(){
+        add(RentalMain);
+        setTitle("Autókereskedés");
+        setResizable(false);
+        setSize(1100, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        String [] colnames = {"Id", "Márka", "Típus", "Ár egy napra"};
+
+        DefaultTableModel rental_model = new DefaultTableModel();
+        rental_model.setColumnIdentifiers(colnames);
+        rental_table.setModel(rental_model);
+
+        rental_table.setRowSelectionAllowed(true);
+        this.btn_rent.setEnabled(false);
+
+        rental_table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if(rental_table.getSelectedRow() > -1){
+                    btn_rent.setEnabled(true);
+                }
+                else{
+                    btn_rent.setEnabled(false);
+                }
+            }
+        });
+    }
+}
